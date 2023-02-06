f9229779bc341497f6b6e6429825681ce0abac4e
capmahin <mahinulhuque1996@gmail.com>
2023-02-07 02:18:51 +0600

    add tailwind classs in layout

--- components/Layout.js
+++ components/Layout.js
@@ -8,19 +8,18 @@ export default function Layout({ title,children}) {
    <Head>
         <title>{title ? title + '- Amazona':'Amazona'}</title>
         <meta name="description" content="Ecommerce Website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <div>
+    <div className="flex min-h-screen flex-col justify-between">
         <header>
-
+header
         </header>
         <main>
          {children}
         </main>
         <footer>
-
+footer
         </footer>
     </div>
     

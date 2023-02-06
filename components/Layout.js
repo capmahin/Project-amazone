a10cb4b1298cb4e6ffa16086680a4c0353cb1f2a
capmahin <mahinulhuque1996@gmail.com>
2023-02-07 02:12:41 +0600

    add title

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,26 @@
+import Head from "next/head"
 import React from 'react'
 
-export default function Layout() {
+export default function Layout({ title,children}) {
   return (
     <>
+
+   <Head>
+        <title>{title ? title + '- Amazona':'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
     <div>
         <header>
 
         </header>
         <main>
-
+         {children}
         </main>
         <footer>
-            
+
         </footer>
     </div>
     
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
+
+// import { Inter } from '@next/font/google'
+import Layout from "@/components/Layout"
 
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-     
-     <h1 className="text-3xl font-bold">Next Tailwind Amazona</h1>
-    </div>
+    <Layout>
+      Home page
+    </Layout>
   )
 }

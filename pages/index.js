45fd402ffd340ce56f83d22bb6e0945217d66089
capmahin <mahinulhuque1996@gmail.com>
2023-02-07 03:26:48 +0600

    solve

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 
 // import { Inter } from '@next/font/google'
 import Layout from "@/components/Layout"
+import ProductItem from "@/components/ProductItem"
+import data from "@/utils/data"
 
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -8,7 +10,11 @@ import Layout from "@/components/Layout"
 export default function Home() {
   return (
     <Layout title='Home Page'>
-      Home page
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product)=>(
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
     </Layout>
   )
 }

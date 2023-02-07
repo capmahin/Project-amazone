968ab49e9cd35de2c5f38834c44ca100f27afa1c
capmahin <mahinulhuque1996@gmail.com>
2023-02-08 02:19:42 +0600

    add StoreProvaider in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import { StoreProvider } from "@/utils/store"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<StoreProvider>
+    <Component {...pageProps} />
+  </StoreProvider>) 
 }

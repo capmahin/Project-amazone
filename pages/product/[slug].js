9f356299f6e2cf266cf8493712f83f119d36fa2f
capmahin <mahinulhuque1996@gmail.com>
2023-02-07 13:14:19 +0600

    add price and card

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -38,6 +38,14 @@ export default function ProductScreen() {
             <li>Description:{product.description}</li>
           </ul>
         </div>
+        <div>
+          <div className="card p-5">
+                 <div className="mb-2 flex justify-between">
+                       <div>Price</div>
+                       <div>${product.price}</div>
+                 </div>
+          </div>
+        </div>
      </div>
     </Layout>
   )

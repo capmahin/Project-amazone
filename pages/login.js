6a4e5ac23be9d7856314e5c263837747fa480509
capmahin <mahinulhuque1996@gmail.com>
2023-02-12 01:57:10 +0600

    add label and input box

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,18 @@
+import Layout from "@/components/Layout"
 import React from 'react'
 
 export default function LoginScreen() {
   return (
-    <div>login</div>
+    <Layout title="Login">
+     <form className="mx-auto max-w-screen-md">
+        <h1 className="mb-4 text-xl">Login</h1>
+        <div className="mb-4">
+        <label htmlFor="email">Email</label>
+        <input type="email" className="w-full">
+            
+        </input>
+        </div>
+     </form>
+    </Layout>
   )
 }

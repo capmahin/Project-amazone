dcfc206d8f07124891ba17d0c5ed958234bd010b
capmahin <mahinulhuque1996@gmail.com>
2023-02-13 01:17:36 +0600

    add session funtion

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,10 @@ export default NextAuth({
         async jwt({token,user}){
             if(user?._id) token._id = user._id;
             if(user?.isAdmin) token.isAdmin= user.isAdmin;
+            return token;
+        },
+        async session({session,token}){
+            if(token?._id)session.user._id= token._id;
         }
     }
 

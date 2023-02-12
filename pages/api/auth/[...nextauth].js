c6f82975aad795a28f80edf71fd04f8276b1e8c4
capmahin <mahinulhuque1996@gmail.com>
2023-02-13 01:12:39 +0600

    add funtion and callback function

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,11 @@ import NextAuth from 'next-auth';
 export default NextAuth({
     session:{
         strategy:'jwt',
+    },
+    callbacks:{
+        async jwt(){
+            
+        }
     }
 
 });

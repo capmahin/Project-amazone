c7f6c8bcfb7c808196089129a213904d39fdc228
capmahin <mahinulhuque1996@gmail.com>
2023-02-14 01:37:06 +0600

    add flex-1

--- components/CheckoutWizard.js
+++ components/CheckoutWizard.js
@@ -5,7 +5,7 @@ export default function CheckoutWizard({activeStep= 0}) {
     <div className="mb-5 flex flex-wrap">
         {
             ['UserLogin','Shipping Address','Payment Method','Place Order'].map((step, index)=> (<div key={step}
-            className={`flex border-b-2 text-center
+            className={`flex-1 border-b-2 text-center
             ${
                 index <= activeStep
                 ?'border-indigo-500 text-indigo-500'

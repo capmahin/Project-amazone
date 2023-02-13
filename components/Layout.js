bc435ee8f7379daafe1ce60dc2b41d243ef519f6
capmahin <mahinulhuque1996@gmail.com>
2023-02-13 14:57:15 +0600

    add signout

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import { Store } from "@/utils/store"
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Head from "next/head"
 import Link from "next/link"
 import React, { useContext, useEffect, useState } from 'react';
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify";
 
 import { Menu } from "@headlessui/react";
 import DropdownLink from "./DropdownLink";
+import Cookies from "js-cookie";
 
 export default function Layout({ title,children}) {
 
@@ -19,7 +20,12 @@ export default function Layout({ title,children}) {
 
   useEffect(()=>{
     setCartItemsCount(cart.cartItems.reduce((a,c)=> a + c.quantity, 0))
-  },[cart.cartItems])
+  },[cart.cartItems]);
+
+  const logoutClickHandler = () =>{
+    Cookies.remove('cart');
+    signOut({callbackUrl:'/login'});
+  }
   return (
     <>
 
@@ -58,6 +64,16 @@ export default function Layout({ title,children}) {
                         <DropdownLink className="dropdown-link" href='/profile'>
                          Profile
                         </DropdownLink>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <DropdownLink className="dropdown-link" href="/order-history">
+                          Order History
+                        </DropdownLink>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <a className="dropdown-link" href="#" onClick={logoutClickHandler}>
+                          LogOut
+                        </a>
                       </Menu.Item>
                     </Menu.Items>
                   </Menu>

0a4e291d5eb592dc1137f020fd6287d601205d31
capmahin <mahinulhuque1996@gmail.com>
2023-02-14 11:50:53 +0600

    ok

--- next.config.js
+++ next.config.js
@@ -6,14 +6,10 @@ const nextConfig = {
 module.exports = nextConfig
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
 
-    return config
-  }
-}
+    return config;
+  },
+};

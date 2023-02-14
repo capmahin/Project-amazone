15bda160a981535c0d89035056bed14bc95ef4b5
capmahin <mahinulhuque1996@gmail.com>
2023-02-15 01:16:19 +0600

    add more conditions

--- pages/payment.js
+++ pages/payment.js
@@ -1,15 +1,31 @@
 import CheckoutWizard from "@/components/CheckoutWizard"
 import Layout from "@/components/Layout"
+import { Store } from "@/utils/store";
 import { useRouter } from "next/router"
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { toast } from "react-toastify";
 
 export default function PaymentScreen() {
     const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('');
+
+    const {state, dispatch} = useContext(Store); 
+    const {cart} = state;
+    const {shippingAddress, paymentMethod} = cart;
     const router = useRouter();
 
     const submitHandler = (e) =>{
         e.preventDefault();
-    }
+        if(!selectedPaymentMethod){
+         return   toast.error('Payment method is required');
+        }
+        dispatch({type: 'SAVE_PAYMENT_METHOD', payload:selectedPaymentMethod});
+    };
+    useEffect(()=>{
+      if(!shippingAddress.address){
+       return  router.push('/shipping');
+      }
+      setSelectedPaymentMethod(paymentMethod || '');
+    },[paymentMethod, router, shippingAddress.address]);
   return (
     <Layout title="Payment Method">
        <CheckoutWizard activeStep={2} />

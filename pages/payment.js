5406c32d99e46613334042a44569aa8de9a6a234
capmahin <mahinulhuque1996@gmail.com>
2023-02-15 00:52:08 +0600

    add CheckoutWizerd

--- pages/payment.js
+++ pages/payment.js
@@ -1,10 +1,11 @@
+import CheckoutWizard from "@/components/CheckoutWizard"
 import Layout from "@/components/Layout"
 import React from 'react'
 
 export default function PaymentScreen() {
   return (
     <Layout title="Payment Method">
-
+       <CheckoutWizard activeStep={2} />
     </Layout>
   )
 }

2dbc0fe9c8b60ed2996d47c73b223a636fb90ea0
capmahin <mahinulhuque1996@gmail.com>
2023-02-15 02:41:45 +0600

    add catagory slug price brand

--- models/Product.js
+++ models/Product.js
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema(
     {
         name:{type: String, required:true},
-        email: {type: String, required: true, unique:true},
-        password: {type: String, required:true},
-        isAdmin: {type: Boolean, required: true, default:false},
+        slug:{type: String, required:true, unique:true},
+        category:{type: String, required:true},
+        image:{type: String, required:true},
+        price:{type:Number, required:true},
+        brand:{type: String, required:true},
+        
     },{
         timeStamps:true,
     }

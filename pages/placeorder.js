83f251217d237c6009d9b01d5d7e55d49a437fcc
capmahin <mahinulhuque1996@gmail.com>
2023-02-16 14:20:00 +0600

    add new funtions in placeorder.js

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -1,12 +1,43 @@
 import CheckoutWizard from "@/components/CheckoutWizard"
 import Layout from "@/components/Layout"
-import React from 'react'
+import { Store } from "@/utils/Store"
+import Link from "next/link"
+import React, { useContext } from 'react'
 
 export default function PlaceOrderScreen() {
+    const {state, dispatch} = useContext(Store);
+    const {cart} = state;
+    const {cartItems, shippingAddress, paymentMethod} = cart;
   return (
    <Layout title="Place Order">
     <CheckoutWizard activeStep={3} />
+    <h1 className="mb-4 text-xl">Place Order</h1>
+    {cartItems.length === 0 ?
+    (
+        <div>
+        cart is empty. <Link legacyBehavior href="/">Go Shopping</Link>
+        </div>
+    ):
+    (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <div className="card p-5">
+              <h2 className="mb-2 text-lg">Shipping Address</h2>
+              <div>
+                {shippingAddress.fullName},{shippingAddress.address},{' '}
+                {shippingAddress.city}, {shippingAddress.postalCode}, {' '}
+                {shippingAddress.country}
+              </div>
+              <div>
+                <Link legacyBehavior href="/shipping">Edit</Link>
+            </div>
+            </div>
+           
 
+          </div>
+        </div>
+    )
+    }
    </Layout>
   )
 }

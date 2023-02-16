05ce26017f405cdbd18f591a2f2cf0a6a4bcc751
capmahin <mahinulhuque1996@gmail.com>
2023-02-16 14:06:40 +0600

    add CheckoutWizard inside layout

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -1,9 +1,11 @@
+import CheckoutWizard from "@/components/CheckoutWizard"
 import Layout from "@/components/Layout"
 import React from 'react'
 
 export default function PlaceOrderScreen() {
   return (
    <Layout title="Place Order">
+    <CheckoutWizard activeStep={3} />
 
    </Layout>
   )

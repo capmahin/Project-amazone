41beca3c7f685b37c41d04bbe070ff2b4272a5ff
capmahin <mahinulhuque1996@gmail.com>
2023-02-20 02:19:54 +0600

    add useRouter in id.js

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,6 +1,15 @@
+import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
 
 function OrderScreen(){
 
+    const {query} = useRouter();
+    const orderId = query.id;
+return (
+    <Layout title={`Order ${orderId}`}>
+
+    </Layout>
+)
 }
 
 OrderScreen.auth = true;

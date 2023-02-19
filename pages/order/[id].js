2459183fd9ad1b17e536b6ab6af314c4891ca63e
capmahin <mahinulhuque1996@gmail.com>
2023-02-20 02:28:27 +0600

    add switch method

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,10 +1,35 @@
 import Layout from "@/components/Layout";
 import { useRouter } from "next/router";
+import { useReducer } from "react";
 
+function reducer (state, action){
+    switch(action.type){
+        case 'FETCH_REQUEST':
+            return {...state, loading:true, error: ''};
+
+        case 'FETCH_SUCCESS':
+            return {...state, loading:false, order:action.payload, error:''};
+
+        case 'FETCH_FAIL':
+            return {...state, loading:false, error:action.payload};
+    }
+}
 function OrderScreen(){
+
+    //order/:id
 
     const {query} = useRouter();
     const orderId = query.id;
+
+    const [
+        {loading, error, order},
+        dispatch,
+    ] = useReducer(reducer,{
+        loading:true,
+        order:{},
+        error:'',
+    });
+    
 return (
     <Layout title={`Order ${orderId}`}>
 

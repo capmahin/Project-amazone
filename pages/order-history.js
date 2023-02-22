76c29a9d165bfccdf1f526f02bab3610afd89b5a
capmahin <mahinulhuque1996@gmail.com>
2023-02-23 03:20:32 +0600

    add a jek request

--- pages/order-history.js
+++ pages/order-history.js
@@ -1,4 +1,5 @@
 import { getError } from "@/utils/error"
+import axios from "axios";
 import React, { useEffect } from 'react'
 
 export default function OrderHistoryScreen() {
@@ -6,6 +7,7 @@ export default function OrderHistoryScreen() {
         const fetchOrders = async ()=>{
             try{
                dispatch({type:'FETCH_REQUEST'});
+               const {data} = await axios.get(`/api/orders/history`);
             } catch(err){
                 dispatch({type: 'FETCH_FAIL', payload:getError(err)});
             }

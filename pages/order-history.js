d70e910537d76614bff41c489cc443290452b394
capmahin <mahinulhuque1996@gmail.com>
2023-02-23 03:17:31 +0600

    add try catch

--- pages/order-history.js
+++ pages/order-history.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function OrderHistoryScreen() {
+    useEffect(()=>{
+        const fetchOrders = async ()=>{
+            try{
+
+            } catch(err){
+                
+            }
+        }
+    },[])
   return (
     <div>order-history</div>
   )

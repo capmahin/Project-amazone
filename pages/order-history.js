88697e58219ae495f39d75230b24388973b9e722
capmahin <mahinulhuque1996@gmail.com>
2023-02-23 03:19:30 +0600

    add new 2 condition

--- pages/order-history.js
+++ pages/order-history.js
@@ -1,12 +1,13 @@
+import { getError } from "@/utils/error"
 import React, { useEffect } from 'react'
 
 export default function OrderHistoryScreen() {
     useEffect(()=>{
         const fetchOrders = async ()=>{
             try{
-
+               dispatch({type:'FETCH_REQUEST'});
             } catch(err){
-                
+                dispatch({type: 'FETCH_FAIL', payload:getError(err)});
             }
         }
     },[])

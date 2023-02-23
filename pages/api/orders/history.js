f010ffd39393ba5ce892b34afd2d4f76fe5edb66
capmahin <mahinulhuque1996@gmail.com>
2023-02-24 02:09:35 +0600

    add res.send

--- pages/api/orders/history.js
+++ pages/api/orders/history.js
@@ -1,3 +1,5 @@
+import Order from "@/models/Order";
+import db from "@/utils/db";
 import { getSession } from "next-auth/react";
 
 
@@ -5,8 +7,12 @@ const handler = async (req,res)=>{
 
     const session = await getSession({req});
     if(!session){
-        return res.status(401).send('signin required');
+        return res.status(401).send({message:'signin required'});
     }
+    const {user} = session;
+    await db.connect();
+    const orders = await Order.find({user:user._id});
+    res.send(orders);
 }
 
 export default handler;
